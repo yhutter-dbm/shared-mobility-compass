4a3868f436f7d028258fe6941eb12a63e2d63559
TheJodok <Jodok.senften@hotmail.com>
2021-03-31 15:40:03 +0200

    MapContainer mit Filter und Map ergänzt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+let map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 47.3769, lng: 8.5417 },
+      zoom: 12,
+      mapId: '533f9f6f21762ace'
+    });
+  }
+
+
 console.log("Hello Shared Mobility Compass");
+
+
