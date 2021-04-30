19161fb4eb9dc4da983d922d98a12c7372f71b25
TheJodok <Jodok.senften@hotmail.com>
2021-04-30 14:09:21 +0200

    added makrer function

--- static/js/index.js
+++ static/js/index.js
@@ -17,6 +17,34 @@ function _initMapBox() {
   });
 }
 
+//Fuction Submit
+//TODO
+
+//Function Get Liste for Markers
+//TODO
+let markierungen = [['markierung1', '7.731537', '47.063618'], ['markierung2', '8.6016', '46.52419'], ['markierung3', '9.54551', '47.3842'], ['markierung4', '9.409512', '47.328598'], ['markierung5', '8.290168', '47.589222']];
+
+
+//Fuction Set Markers
+function _setmarkers (markierungen) {
+  for (let i = 0; i < markierungen.length; i++) {
+    console.log(markierungen[i][0]);
+    var marker =  new mapboxgl.Marker({ color: 'black', rotation: 45 })
+        .setLngLat([markierungen[i][1], markierungen[i][2]])
+        .addTo(map);
+    markers.push(marker);
+  };
+}
+
+//Fuction clear Markers
+function _clearmarkers (markers) {
+  for (let i = 0; i < markers.length; i++) {
+      markers[i].remove();
+  };
+  markers = [];
+}
+
+
 // Get reference to button elements and register click handlers
 const searchButton = $("#searchButton");
 const searchContainer = $("#searchContainer");
@@ -27,3 +55,6 @@ const filterContainer = $("#filterContainer");
 filterButton.click(() => _openFilter());
 
 _initMapBox();
+
+var markers = [];
+_setmarkers(markierungen);
