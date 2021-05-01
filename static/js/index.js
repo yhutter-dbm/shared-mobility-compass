65b0925922298036297a21a620c1459e18b6c345
TheJodok <Jodok.senften@hotmail.com>
2021-05-01 08:55:32 +0200

    Changed Marker function

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+let map = {};
+
 function _openSearch() {
   searchContainer.toggleClass("open");
 }
@@ -9,15 +11,15 @@ function _openFilter() {
 function _initMapBox() {
   mapboxgl.accessToken =
     "pk.eyJ1IjoieWFubmlja2h1dHRlciIsImEiOiJja24wamRwa3QwZGQyMnVscm4zOGIxN2V0In0.978_7epIiz2f4Wd26ABpwg";
-  const map = new mapboxgl.Map({
+  map = new mapboxgl.Map({
     container: "mapContainer", // container ID
-    style: "mapbox://styles/mapbox/dark-v9", // style URL
+    style: "mapbox://styles/mapbox/dark-v10", // style URL
     center: [8.5417, 47.3769], // starting position [lng, lat]
     zoom: 12, // starting zoom
   });
 }
 
-//Fuction Submit
+//Function Submit
 //TODO: Wenn Submit geklickt wird, schick plz und zoom stufe an flask
 //TODO2: Wenn Submit geklickt wird ganzes Formular mitschicken, damit liste anhand der Filter einstellungen erstellt werden kann
 
@@ -26,11 +28,11 @@ function _initMapBox() {
 let markierungen = [['markierung1', '7.731537', '47.063618'], ['markierung2', '8.6016', '46.52419'], ['markierung3', '9.54551', '47.3842'], ['markierung4', '9.409512', '47.328598'], ['markierung5', '8.290168', '47.589222']];
 
 
-//Fuction Set Markers
+//Fuction Set Markers - SEJO
 function _setmarkers (markierungen) {
   for (let i = 0; i < markierungen.length; i++) {
     console.log(markierungen[i][0]);
-    var marker =  new mapboxgl.Marker({ color: 'black', rotation: 45 })
+    var marker =  new mapboxgl.Marker({ color: 'red', rotation: 45 })
         .setLngLat([markierungen[i][1], markierungen[i][2]])
         .addTo(map);
     markers.push(marker);
@@ -38,7 +40,7 @@ function _setmarkers (markierungen) {
 }
 //TODO: Marker hoverable machen, damit zusatz Infos angezegit werden
 
-//Fuction clear Markers
+//Fuction clear Markers - SEJO
 function _clearmarkers (markers) {
   for (let i = 0; i < markers.length; i++) {
       markers[i].remove();
