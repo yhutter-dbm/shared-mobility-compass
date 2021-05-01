5e3117ba8ac86214525631a73304496da3308075
YahArt <hutteryannick@gmail.com>
2021-05-01 18:12:23 +0200

    Add vectorized haversine function

--- services/station_service.py
+++ services/station_service.py
@@ -2,6 +2,7 @@ from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 from flask import json
 import pandas as pd
+import numpy as np
 import os
 
 class StationService:
@@ -19,8 +20,28 @@ class StationService:
 			json_data = json.load(json_file)
 		return pd.json_normalize(json_data["data"]["stations"])
 
+
+	# Vectorized haversine function from: https://stackoverflow.com/questions/29545704/fast-haversine-approximation-python-pandas
+	def haversine(self, lon1, lat1, lon2, lat2):
+		"""
+		Calculate the great circle distance between two points
+		on the earth (specified in decimal degrees)
+
+		All args must be of equal length.    
+
+		"""
+		lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+		dlon = lon2 - lon1
+		dlat = lat2 - lat1
+
+		a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+		c = 2 * np.arcsin(np.sqrt(a))
+		km = 6367 * c
+		return km
+
 	def stations_from_location(self, address, radius):
 		location = self.geolocator.geocode(address)
 		# Optimization for querying stations in range, implemented with reference to: https://engineering.upside.com/a-beginners-guide-to-optimizing-pandas-code-for-speed-c09ef2c6a4d6#:~:text=Vectorization%20is%20the%20process%20of,check%20out%20the%20Pandas%20docs)
-		# Sadly a vectorization approach is not possible as the geodesic function does not seem to support arrays of tubles...
-		return self.station_information[self.station_information.apply(lambda row: geodesic((row['lat'], row['lon']), (location.latitude, location.longitude)).km <= radius, axis=1)]
+		return self.station_information[self.haversine(location.latitude, location.longitude, self.station_information['lat'], self.station_information['lon']) <= radius]
